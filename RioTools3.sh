6217245767219b1a33b119849c84616e18e694ca
MR•RIO-SPMX <113223024+MRRIOSPMX@users.noreply.github.com>
2022-09-10 10:23:12 +0700

    Add files via upload

---
+++ RioTools3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+figlet TROJANS
+echo -e  $yellow"===========================================================================" | lolcat
+echo -e  $blue" {+} PEMBUAT : RIO TINTO JUNIOR"
+echo -e  $blue" {+} TEAM :MAFIA TEKNOLOGI"
+echo -e  $blue" {+} PESAN : GUNAKAN DENGAN BIJAK"
+echo -e  $yellow"===========================================================================" | lolcat
+read -p "Masukan No Target : " nomor;
+
+while [ true ]; do
+echo -e $green"Mengirim Virus Ke Nomor $nomor"
+done
